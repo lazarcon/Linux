8991bad6d699d6ccb6665810a2c42534205738a0
lazarcon <constantin.lazari@gmail.com>
2019-05-11 12:45:28 +0200

    > /dev/null removed

---
+++ backup/backupToGit.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+###########
+
+########################
+# Variables
+########################
+
+# Repositories to backup
+repos="/home/cola/Git/TeX /home/cola/Git/Java /home/cola/Git/LUA /home/cola/Git/Android /home/cola/Workspaces/Linux";
+# Remember current directory
+currentDirectory=$(pwd);
+# create current date variable
+today=$(date '+%Y-%m-%d');
+# create update message
+message="Auto update from $today";
+######################
+# Logic
+######################
+echo "Backing up repositories:"
+for repo in $repos
+do
+    echo -n "   Pushing $repo ...";
+    cd $repo;
+    git add -A;
+    git commit m "$message";
+    git push;
+    echo " done"
+done
+cd $currentDirectory
+
