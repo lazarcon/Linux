5f2b53f54d9f1126c54c05a3be95729cdc358f80
lazarcon <constantin.lazari@gmail.com>
2020-03-08 08:42:41 +0100

    Script for mounting Samba

---
+++ createAndMountSambaMusic.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+echo "Installing sshfs and fuse:"
+sudo apt install sshfs fuse
+
+echo "Creating fuse group:
+sudo groupadd fuse
+
+
+echo "Adding user to group"
+sudo adduser cola fuse
+
+echo "Creating Mount Target"
+mkdir ~/SambaMusic
+
+echo "Mounting SambaMusic"
+sshfs pi@raspberrypi:/media/usbmusic/music ~/SambaMusic
+
+echo "Changing into Directory"
+cd ~/SambaMusic
+
+
