183480f91c0b517656afa105eeeec1d581ea539a
lazarcon <constantin.lazari@gmail.com>
2021-02-06 10:49:47 +0100

    Auto update from 2021-02-06

--- backup/backup.sh
+++ backup/backup.sh
@@ -63,7 +63,7 @@ BACKUP_NTFS=$MOUNT_POINT/Backups/$MACHINE/NTFS;
 #source ./jobs/startBackupServer.sh $BACKUP_SERVER_NAME $BACKUP_SERVER_MAC
 
 # Mount the Backup-Server:
-#source $ROOT_FOLDER/jobs/mountBackupServer.sh $BACKUP_SERVER_IP $MOUNT_POINT
+source $ROOT_FOLDER/jobs/mountBackupServer.sh $BACKUP_SERVER_IP $MOUNT_POINT
 
 # Backup system files
 source $ROOT_FOLDER/jobs/backupDatabases.sh $BACKUP_FOLDER
